ab7fb14f0843616190f80cc9889844f66cb3b4dd
hstt <eduardolopes19@outlook.com>
2022-04-22 14:03:06 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(2, 5);
-        System.out.println(position);
+        Board board = new Board(8,8);
     }
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -27,6 +27,6 @@ public class Position {
 
     @Override
     public String toString() {
-        return row + ", " + column;
+        return row + ",  " + column;
     }
 }
