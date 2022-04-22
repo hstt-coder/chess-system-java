8a9976a2a2fa6b48f7e449f0df06e908640b4c6f
hstt <eduardolopes19@outlook.com>
2022-04-22 13:49:17 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(2, 5);
+        System.out.println(position);
     }
 }
