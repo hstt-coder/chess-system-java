ca8354440577156f1519bde97db54653207b89c8
hstt <eduardolopes19@outlook.com>
2022-04-22 21:04:55 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,8 +35,10 @@ public class ChessMatch {
     }
 
     private void validateSourcePosition(Position position) {
-        if(!board.thereIsAPiece(position))
+        if (!board.thereIsAPiece(position))
             throw new ChessException("There is no piece on source position.");
+        if (!board.piece(position).isThereAnyPossibleMove())
+            throw new ChessException("There is no possible moves for the chosen piece.");
     }
 
     private Piece makeMove(Position source, Position target) {
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -10,10 +10,14 @@ public class Rook extends ChessPiece {
         super(board, color);
     }
 
-
-
     @Override
     public String toString() {
         return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
