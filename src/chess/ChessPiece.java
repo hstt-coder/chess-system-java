7067ec2d184b88e96640b23710b8e1749fce39e0
hstt <eduardolopes19@outlook.com>
2022-04-23 22:04:54 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,19 +7,22 @@ import chess.enums.Color;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
 
-    public Color getColor() {
-        return color;
-    }
+    public Color getColor() { return color; }
 
-    public ChessPosition getChessPosition() {
-        return ChessPosition.fromPosition(position);
-    }
+    public int getMoveCount() { return moveCount; }
+
+    public void increaseMoveCount() { moveCount++; }
+
+    public void decreaseMoveCount() { moveCount--; }
+
+    public ChessPosition getChessPosition() { return ChessPosition.fromPosition(position); }
 
     protected boolean isThereAnyOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
