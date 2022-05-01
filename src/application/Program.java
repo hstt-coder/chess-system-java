10575a97fc339ec358ba7067c7d23a0f69d63ec4
hstt <eduardolopes19@outlook.com>
2022-04-30 21:58:35 -0300

    FIX - better treatment for promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,7 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.exceptions.ChessException;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
@@ -35,9 +32,16 @@ public class Program {
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
                 if (capturedPiece != null)
                     captured.add(capturedPiece);
+
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Enter piece for promotion (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Invalid value!\nEnter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+
                     chessMatch.replacePromotedPiece(type);
                 }
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,7 +7,6 @@ import chess.enums.Color;
 import chess.exceptions.ChessException;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -113,7 +112,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
 
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q"))
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
 
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
